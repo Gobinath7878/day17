38bddf8bd62fd7e5c9df42782672eacadc680e64
Gobinath S <115769414+Gobinath7878@users.noreply.github.com>
2022-11-16 10:47:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,36 +2,91 @@
 
 setTimeout(()=>{
   document.getElementById("demo").innerHTML="10";
-},1000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="9";
-},2000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="8";
-},3000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="7";
-},4000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="6";
-},5000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="5";
-},6000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="4";
-},7000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="3";
-},8000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="2";
-},9000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="1";
-},10000);
-setTimeout(()=>{
-  document.getElementById("demo").innerHTML="Happy New Year!";
-},11000);
+
+   setTimeout(()=>{
+     document.getElementById("demo").innerHTML="9";
+
+      setTimeout(()=>{
+        document.getElementById("demo").innerHTML="8";
+
+       setTimeout(()=>{
+          document.getElementById("demo").innerHTML="7";
+
+        setTimeout(()=>{
+          document.getElementById("demo").innerHTML="6";
+
+           setTimeout(()=>{
+            document.getElementById("demo").innerHTML="5";
+
+            setTimeout(()=>{
+              document.getElementById("demo").innerHTML="4";
+
+              setTimeout(()=>{
+                document.getElementById("demo").innerHTML="3";
+
+                setTimeout(()=>{
+                  document.getElementById("demo").innerHTML="2";
+
+                    setTimeout(()=>{
+                     document.getElementById("demo").innerHTML="1";
+                       
+                        setTimeout(()=>{
+                         document.getElementById("demo").innerHTML="Happy New Year!";
+                     
+                        },11000);
+                      },10000);
+                    },900);
+                  },800);
+                },700);
+              },600);
+            },500);
+          },400);
+        },300);
+      },200);
+},100);
+
+
+
+
+
+
+
+
+
+
+
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="10";
+// },1000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="9";
+// },2000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="8";
+// },3000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="7";
+// },4000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="6";
+// },5000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="5";
+// },6000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="4";
+// },7000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="3";
+// },8000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="2";
+// },9000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="1";
+// },10000);
+// setTimeout(()=>{
+//   document.getElementById("demo").innerHTML="Happy New Year!";
+// },11000);
 
 
